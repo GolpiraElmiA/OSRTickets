356798a47f22d1fc8d7b86f944121872c9a02b4a
GolpiraElmiA <glp.elmi@gmail.com>
2025-02-18 12:07:10 -0700

    Update OSRTickets.py

--- OSRTickets.py
+++ OSRTickets.py
@@ -64,9 +64,7 @@ def load_data():
 # Initialize or load data into session state
 if "df" not in st.session_state:
     st.session_state.df = load_data()
-    # st.session_state.df.rename(columns={"Department": "Section"}, inplace=True) # df=######### 18Feb
-
-
+    
     ############################
 
 # Reset functionality
@@ -115,6 +113,7 @@ with st.form("add_ticket_form"):
             "Summary": summary,
         }
         st.session_state.df = pd.concat([st.session_state.df, pd.DataFrame([new_ticket])], ignore_index=True)
+        st.session_state.df=st.session_state.df.drop(columns=['Sections'] ## 18 FEB 2025*********
         save_to_drive(st.session_state.df, 'StatisticalAnalysisTickets.csv')
         st.success(f"Ticket {ticket_id} has been submitted successfully!")
 
